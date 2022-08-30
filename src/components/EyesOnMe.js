fba3413ac90ab473937e741a5178d9ea4541778e
Austin Barrington <arbarrington12@gmail.com>
2022-08-30 15:55:06 -0600

    commit tree 97d0c82ee913bf8a99f47c3d702065e6bd3abb05

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    function handleBlur() {
+        console.log("Hey! Eyes on me!");
+    }
+
+
+    function handleFocus() {
+        console.log("Good!");
+    }
+    
+    return (
+        <div>
+            <button type="button" onBlur={handleBlur} onFocus={handleFocus}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handlePassword(e) {
+        e.preventDefault();
+        console.log('Entering password...');
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" name="keypad" onChange={handlePassword}></input>
+        </div>
     )
 }
 
